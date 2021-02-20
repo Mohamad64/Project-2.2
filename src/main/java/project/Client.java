e457bd4b7e26afb132c6f7127acfe6d919742208
Mohamad64 <49920290+Mohamad64@users.noreply.github.com>
2021-02-20 22:16:33 +0000

    God bless patience

---
+++ src/main/java/project/Client.java
@@ -0,0 +1,31 @@
+package project;
+
+import java.io.Serializable;
+import java.util.function.Consumer;
+
+public class Client extends Connections {
+
+    private String ip;
+    private int port;
+
+    public Client(String ip, int port, Consumer<Serializable> onReceiveCallback) {
+        super(onReceiveCallback);
+        this.ip = ip;
+        this.port = port;
+    }
+
+    @Override
+    protected boolean isServer() {
+        return false;
+    }
+
+    @Override
+    protected String getIP() {
+        return ip;
+    }
+
+    @Override
+    protected int getPort() {
+        return port;
+    }
+}
