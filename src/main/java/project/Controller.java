1851e64953293aeb34a20432b69107fc61a60337
Ramón Reszat <ramon@reszat.de>
2021-02-20 21:10:46 +0100

    Add Vbox with TextField

---
+++ src/main/java/project/Controller.java
@@ -0,0 +1,23 @@
+package project;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+//    @Override
+//    public void init() throws Exception {
+//    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
+    public void setTxtFieldAction() {
+    }
+
+    public void setHamAction() {
+    }
+}
