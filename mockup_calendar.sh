2fa025d14fe2fac1497acdb38173ddde51fa4a25
DanielTossaint <56160943+DanielTossaint@users.noreply.github.com>
2021-02-24 14:44:33 +0100

    json files for skills

--- mockup_calendar.sh
+++ mockup_calendar.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 mongoimport --db calendar --collection courses --drop --file datasets/courses.json
+mongoimport --db calendar --collection rooms --drop --file datasets/rooms.json
+mongoimport --db calendar --collection lectures --drop --file datasets/lectures.json
