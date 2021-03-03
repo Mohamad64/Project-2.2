fc92755976fde080c24919628004651a2e0ba870
Ramón Reszat <ramon@reszat.de>
2021-03-03 11:23:39 +0100

    Add use skill

--- src/main/java/project/Database/SkillsDatabase.java
+++ src/main/java/project/Database/SkillsDatabase.java
@@ -1,14 +1,20 @@
 package project.Database;
 
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoIterable;
 
 public class SkillsDatabase {
 
     private MongoClient local;
+    private MongoDatabase skill;
 
     public SkillsDatabase(){
         local = new MongoClient("localhost", 27017);
+    }
+
+    public void useSkill(String skillName){
+        skill = local.getDatabase(skillName);
     }
 
     public MongoIterable<String> getDatabaseNames(){
