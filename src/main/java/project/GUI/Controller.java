3913995a987cdaa2f4ac92336b0ad3601ce05815
Mohamad64 <49920290+Mohamad64@users.noreply.github.com>
2021-03-14 18:04:12 +0100

    TextEditor

--- src/main/java/project/GUI/Controller.java
+++ src/main/java/project/GUI/Controller.java
@@ -44,8 +44,8 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
 
     Pane box;
-    private final static String[] weekdays = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
-    public static String inquiry;
+//    private final static String[] weekdays = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+//    public static String inquiry;
 
 
     /*
@@ -60,7 +60,7 @@ public class Controller implements Initializable {
     public Label label1;
     public Button update;
     private boolean isServer = false;
-    private Connections connection = isServer ? createServer() : createClient();
+    private project.GUI.Connections connection = isServer ? createServer() : createClient();
     private DateFormat df = new SimpleDateFormat("hh:mm");
 
 
@@ -156,48 +156,48 @@ public class Controller implements Initializable {
         });
     }
 
-    private static void inquire(String message) {
-
-        if(message.contains("time") && message.contains("course")) {
-            inquiry += "<time>";
-            String[] split = message.split(" ");
-            int index = 0;
-            for(String s : split) {
-                if(s.equals("course"))
-                    break;
-                index++;
-            }
-            inquiry+='<'+split[index+1]+'>';
-            index=0;
-            for (String ss:split){
-                if(ss.equals("on"))
-                    break;
-                index++;
-            }
-            inquiry+='<'+split[index+1]+'>';
-        }
-
-        if(message.contains("course")||message.contains("courses")) {
-            inquiry += "<course>";
-            for(String s:weekdays){
-                if(message.contains(s))
-                    inquiry+='<'+s+'>';
-            }
-        }
-
-        if(message.contains("courses") && message.contains("date")){
-            inquiry+="<course>";
-            String[] split = message.split(" ");
-            int index=0;
-            for(String s:split){
-                if(s.equals("date"))
-                    break;
-                index++;
-            }
-            inquiry+='<'+split[index+1]+'>';
-        }
-        inquiry = inquiry.trim();
-    }
+//    private static void inquire(String message) {
+//
+//        if(message.contains("time") && message.contains("course")) {
+//            inquiry += "<time>";
+//            String[] split = message.split(" ");
+//            int index = 0;
+//            for(String s : split) {
+//                if(s.equals("course"))
+//                    break;
+//                index++;
+//            }
+//            inquiry+='<'+split[index+1]+'>';
+//            index=0;
+//            for (String ss:split){
+//                if(ss.equals("on"))
+//                    break;
+//                index++;
+//            }
+//            inquiry+='<'+split[index+1]+'>';
+//        }
+//
+//        if(message.contains("course")||message.contains("courses")) {
+//            inquiry += "<course>";
+//            for(String s:weekdays){
+//                if(message.contains(s))
+//                    inquiry+='<'+s+'>';
+//            }
+//        }
+//
+//        if(message.contains("courses") && message.contains("date")){
+//            inquiry+="<course>";
+//            String[] split = message.split(" ");
+//            int index=0;
+//            for(String s:split){
+//                if(s.equals("date"))
+//                    break;
+//                index++;
+//            }
+//            inquiry+='<'+split[index+1]+'>';
+//        }
+//        inquiry = inquiry.trim();
+//    }
 
     public void confirmAction(ActionEvent actionEvent) {
         Client.setName(username.getText());
