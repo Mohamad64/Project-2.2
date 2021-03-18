c18f788d2ecf950c24021873833005221dd9f7aa
Ramón Reszat <ramon@reszat.de>
2021-03-18 20:59:59 +0100

    Make commands imperative

--- src/main/java/project/Database/SkillsDatabase.java
+++ src/main/java/project/Database/SkillsDatabase.java
@@ -21,25 +21,38 @@ public class SkillsDatabase {
     private MongoClient local;
     private MongoDatabase skill;
 
+    private MongoCollection<Document> collection;
+    private ArrayList<Bson> commands;
+
     public SkillsDatabase(){
         local = new MongoClient("localhost", 27017);
+        commands = new ArrayList<Bson>();
     }
 
     public void useSkill(String skillName){
         skill = local.getDatabase(skillName);
     }
 
-    protected Bson listing(String leftCollection, String rightCollection, String keyName){
-        return lookup(rightCollection, "_id", keyName, rightCollection);
+    public void load(String collectionName){
+        this.collection = skill.getCollection(collectionName);
     }
 
-    protected Bson contains(String key, Object value) {
-        return match(eq(key,value));
+    protected void listing(String leftCollection, String rightCollection, String keyName){
+        this.commands.add(lookup(rightCollection, "_id", keyName, rightCollection));
     }
 
-    protected List<Document> get(String collectionName,  List<Bson> pipeline){
-        MongoCollection<Document> collection = skill.getCollection(collectionName);
-        return collection.aggregate(pipeline).into(new ArrayList<>());
+    protected void contains(String key, Object value) {
+        this.commands.add(match(eq(key,value)));
+    }
+
+    protected void show(String fieldName) {
+        this.commands.add(project(fields(excludeId(), include(fieldName))));
+    }
+
+    protected List<Document> get(){
+        List<Document> results =  this.collection.aggregate(commands).into(new ArrayList<>());
+        this.commands = new ArrayList<Bson>();
+        return results;
     }
 
     protected MongoIterable<String> getDatabaseNames(){
@@ -54,22 +67,21 @@ public class SkillsDatabase {
     public static void main(String[] args){
         SkillsDatabase db = new SkillsDatabase();
 
-        //testing commands to get data from the skill
-        ArrayList<Bson> commands = new ArrayList<Bson>();
-
         db.useSkill("calendar");
+        
+        //look at the courses
+        db.load("courses");
 
         //chaining the necessary commands to combine collections
-        commands.add(db.listing("courses", "lectures", "course_id"));
+        db.listing("courses", "lectures", "course_id");
         //commands.add(db.contains("course-name","Mathematical Modelling"));
-        commands.add(db.contains("lectures.start_time","2021-03-05T13:45:00Z"));
+        db.contains("lectures.start_time","2021-03-05T13:45:00Z");
 
         //optionally only show the fields you specify
-        Bson project = project(fields(excludeId(), include("course-name")));
-        commands.add(project);
+        db.show("course-name");
 
         //output retrieved documents in JSON format
-        List<Document> results = db.get("courses", commands);
+        List<Document> results = db.get();
         System.out.println(results.toString());
     }
 }
