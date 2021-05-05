bd076009d554323f2cca81073144a300367dc8e7
irfan <ikrdnz@gmail.com>
2021-05-05 17:21:21 +0200

    face detection application

---
+++ src/main/java/project/GUI/FaceDetection.java
@@ -0,0 +1,59 @@
+package project.GUI;
+
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import org.opencv.core.Core;
+
+
+public class FaceDetection extends Application
+{
+    @Override
+    public void start(Stage primaryStage)
+    {
+        try
+        {
+            // load the FXML resource
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/FaceDetection.fxml"));
+            BorderPane root = (BorderPane) loader.load();
+            // set a whitesmoke background
+            root.setStyle("-fx-background-color: whitesmoke;");
+            // create and style a scene
+            Scene scene = new Scene(root, 800, 600);
+//            scene.getStylesheets().add(getClass().getResource("fxml/application.css").toExternalForm());
+            // create the stage with the given title and the previously created
+            // scene
+            primaryStage.setTitle("Face Detection and Tracking");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+            // init the controller
+            FaceDetectionController controller = loader.getController();
+            controller.init();
+
+            primaryStage.setOnCloseRequest((new EventHandler<WindowEvent>() {
+                public void handle(WindowEvent we)
+                {
+                    controller.setClosed();
+                }
+            }));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        // load the native OpenCV library
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        launch(args);
+    }
+}
