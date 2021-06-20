172df3ccefc2e6639918da804fa27e315022619c
DanielTossaint <56160943+DanielTossaint@users.noreply.github.com>
2021-06-20 15:29:09 +0200

    right tojava

--- src/main/java/project/NLP/ToJava.java
+++ src/main/java/project/NLP/ToJava.java
@@ -3,22 +3,23 @@ package project.NLP;
 import org.pmml4s.model.Model;
 
 
+
 import java.nio.file.Paths;
 import java.util.*;
 
 public class ToJava {
-    private final Model model = Model.fromFile(String.valueOf(Paths.get("Models/model.pmml")));
+    private final Model model = Model.fromFile(String.valueOf(Paths.get("Models/svc_tfidf.pmml")));
 
-    public Double getRegressionValue(String input) {
+    public Object getRegressionValue(String input) {
         Object[] valuesMap = {input};
         Object[] result = model.predict(valuesMap);
-        return (Double) result[0];
+        return result[0];
     }
 
     public static void main(String[] args) {
         ToJava main = new ToJava();
-        String input = "Where is Madrid located";
-        double predicted = main.getRegressionValue(input);
+        String input = "How do I get to SpaceBox";
+        Object predicted = main.getRegressionValue(input);
         System.out.println(predicted);
     }
 }
